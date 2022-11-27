d3da94bad8ef79262fb7dc6edb80df8f5760ec8c
Aarish <aarishm224@gmail.com>
2022-11-27 19:21:01 +0530

    Spread Operator Completed

---
+++ spread_operator.js
@@ -0,0 +1,23 @@
+// Spread Operator and Destructuring
+arr = [1, 2, 4, 6, 32]
+
+function avg(a, b, c) {
+    return a+b+c/3
+}
+
+// let a = avg(...arr)
+// let a = [4, 7, ...arr, 67]
+// let a = [...arr]
+// let a2 = [5, 7, 9];
+// let a = [...arr, ...a2]
+let obj1 = {
+    name: "Aarish",
+    class: "8-A",
+    favlang: "JavaScript"
+}
+// let obj2 = {}
+// obj2.name = obj1.name
+// obj2.class = obj1.class
+// obj2.favlang = "Python"
+// let obj2 = {...obj1, favlang: "Python"}
+// console.log(obj2)
