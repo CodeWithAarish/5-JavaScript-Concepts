fc39e04deb1d84503a195ff89217262042a69657
CodeWithAarish <aarishm224@gmail.com>
2022-11-28 15:40:43 +0530

    Filter Completed

--- 04_map_filter_reduce.js
+++ 04_map_filter_reduce.js
@@ -1,7 +1,14 @@
 // JavaScript Map
-let a = [1, 2, 3, 4, 5];
+let a = [1, 2, 3, 4, 5, 64, 34, 12, 9];
 let a2 = a.map((value)=>{
     return value * 2;
 })
 
-console.log(a, a2)
+// console.log(a, a2)
+
+// Javascript Filter
+let a3 = a.filter((value)=>{
+    return value > 6;
+})
+console.log(a, a3)
+// Javascript Reduce
